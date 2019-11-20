c8df838a44bbd8e5b4d1639d3ef0dc9a88be4d8e
oggyu <oggyu77@gmail.com>
2019-11-20 19:34:48 +0900

    show image and detect circle in the pipe

--- makePipe.m
+++ makePipe.m
@@ -1,4 +1,21 @@
-x = 0:0.1:2*pi;
-plot(cos(x),sin(x));
+clear
+clc
 
-% todo check it with new picture 
+pipImg = imread('/home/oggyu/Development/MatlabDev/PipeProject/image/pipe2.jpg');
+pipImg = rgb2gray(pipImg);
+
+% pipImg = edge(pipImg,'canny');
+
+[centers, radii, metric] = imfindcircles(pipImg,[1 15]);
+
+imshow(pipImg);
+
+circleCenter = centers(1:3,:);
+circleRadius = radii(1:3);
+
+
+viscircles(circleCenter,circleRadius,'EdgeColor','r'); 
+
+
+
+% todo check it with new picture %
